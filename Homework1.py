f003db6997d81e722f00e1ff312c814e83f8515e
Denis <wishvrn@gmail.com>
2020-11-04 14:24:11 +0300

    Выполнена ДЗ1

---
+++ Homework1.py
@@ -0,0 +1,9 @@
+print ("Создаем несколько переменных")
+a = 1
+b = a + 1
+c = "some text"
+print("Выводим созданные переменные:", a, b, c)
+print("Запрашиваем имя отчество пользователя:")
+first_name = input("Ваше имя: ")
+second_name = input("Введите ваше отчество: ")
+print("Здравствуйте ", first_name, second_name)
